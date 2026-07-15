0f514588e6c149cf30e7b6e88b39eb87fc1ee59b
nicholas-grub <nicholastengky.26@ichat.sp.edu.sg>
2026-07-15 14:58:48 +0800

    Update IEPPROJECT.ino

--- IEPPROJECT/IEPPROJECT.ino
+++ IEPPROJECT/IEPPROJECT.ino
@@ -1,6 +1,11 @@
+#define LED
+#define KNOB A0 // (Blue) Potentionometer Knob
+#define CLK 10
+#define DIO 11
+TM1637 disp(CLK,DIO);
+
 void setup() {
-  // put your setup code here, to run once:
-my balls
+}
 void loop() {
   // put your main code here, to run repeatedly:
 Serial.print("poop");
