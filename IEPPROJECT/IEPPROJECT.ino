9459ecf1d037d58f0615a51fdd3f4f8563cd528a
nicholas-grub <nicholastengky.26@ichat.sp.edu.sg>
2026-07-22 14:42:29 +0800

    Update IEPPROJECT.ino

--- IEPPROJECT/IEPPROJECT.ino
+++ IEPPROJECT/IEPPROJECT.ino
@@ -11,8 +11,6 @@
 #define CLK 10
 #define DIO 11
 TM1637 disp(CLK, DIO);
-int buttonstate1 = 0;
-int buttonstate2 = 0;
 int arrayLED[4] = {7,6,5,4 };
 void LEDselect(int y, int b, int g, int r){
     digitalWrite(y, HIGH);
@@ -21,11 +19,14 @@ void LEDselect(int y, int b, int g, int r){
     digitalWrite(r, LOW);}
 int checkbuttons();
 int readbuttons();
+int choosingmedicine();
 int buttons = 0;
 
-    
+
+   
 int checkbuttons(){
   int prev,curr;
+
 
   curr = readbuttons();
   do {
@@ -36,6 +37,7 @@ int checkbuttons(){
   return curr;
 }
 
+
 int readbuttons(){
   int k1,k2;
   k1 = !digitalRead (K1PIN);
@@ -43,9 +45,10 @@ int readbuttons(){
   return(k1 | k2 <<1);
 }
 
-//Button Toggle
-bool prevButtonState = false;
-int currButtonState;
+
+
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -58,29 +61,18 @@ void setup() {
   pinMode(K2PIN, INPUT_PULLUP);
   pinMode (BUZZER, OUTPUT);
   disp.init();
-
-  //Button Toggle
-  bool currButtonState = digitalRead(K2PIN);
 }
 
 
+
+
 void loop() {
-
-//Button Toggle
-prevButtonState = currButtonState;
-currButtonState = digitalRead(K2PIN);
-
-
  
   int button = checkbuttons();
-
-  int knobValue = analogRead(KNOB); 
-  int knobLEDselect = map(knobValue, 0, 1023, 0, 100); 
-  int knobTIMEselect = map(knobValue, 0, 1023, 0, 24);  
+  int knobValue = analogRead(KNOB);
+  int knobLEDselect = map(knobValue, 0, 1023, 0, 100);  
   int displayselect = map(knobLEDselect, 0, 100, 0, 4);  
 
-  buttonstate1 = digitalRead (K1PIN);
-  buttonstate2 = digitalRead (K2PIN);
 
   if (button == 2){
 
@@ -90,21 +82,22 @@ currButtonState = digitalRead(K2PIN);
   Serial.print(" ");
   Serial.print(knobLEDselect);
   Serial.println(" ");
-
-if (currButtonState == LOW && prevButtonState == HIGH) {
   disp.display(displayselect + 1);
+
+
   if (knobLEDselect<=25){
     LEDselect(7, 6, 5, 4);}
   else if (knobLEDselect>25 ;knobLEDselect<=50){
     LEDselect(6, 5, 4, 7);}
   else if (knobLEDselect>50 ; knobLEDselect<=75){
     LEDselect(5, 4, 7, 6);}
-  else if (knobLEDselect>75;knobLEDselect<=100){
+  else if ( knobLEDselect>75;knobLEDselect<=100){
     LEDselect(4, 7, 6, 5);}
-}
-  prevButtonState = currButtonState;
-  
+
+
   }
+
+
 
 }
 
